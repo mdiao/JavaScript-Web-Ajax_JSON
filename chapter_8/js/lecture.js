33198810992b7de1d96c9b4cc04c6ae7c897bcbe
mdiao <badiawo@gmail.com>
2017-03-16 16:52:16 -0400

    Updating the file for the lecture.js

--- chapter_8/js/lecture.js
+++ chapter_8/js/lecture.js
@@ -1,4 +1,11 @@
 
-ajaxGet("http://localhost:8888/javascript-web-srv/data/langages.txt", function(response){
-    console.log(response);
+ajaxGet("http://localhost:8888/javascript-web-srv/data/films.json", function(reponse){
+    
+    //Transform the char to an object
+    var films = JSON.parse(reponse);
+    //Display the title of each movie
+    films.forEach(function (film){
+        console.log(film.tire);
+    })
+    
 });
